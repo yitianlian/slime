2216dc97dc1f007db4d09e81f2754dde0f04e9d3
yitianlian <xiechengxing34@gmail.com>
2025-10-06 02:11:37 +0000

    revise default args

--- slime/utils/arguments.py
+++ slime/utils/arguments.py
@@ -672,7 +672,7 @@ def get_slime_extra_args_provider(add_custom_arguments=None):
                 "--slime-router-middleware-paths",
                 type=str,
                 nargs="+",
-                default="slime.router.middleware_hub.radix_tree_middleware.RadixTreeMiddleware",
+                default=None,
             )
             return parser
 
